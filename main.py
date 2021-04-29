ad0acd67738bb106e5c571ebab3acf79f1283db8
ovad4221 <andrey22ovs@yandex.ru>
2021-04-29 20:44:33 +0300

    Таймер-бот

--- main.py
+++ main.py
@@ -1,45 +1,28 @@
 import discord
 from discord.ext import commands
+import time
 from requests import get
+import datetime
 import asyncio
 
-TOKEN = "ODM3MzYxNzAzNjUyOTUwMDU4.YIrb2w.OJLw5m_8JoWcKBBc7qRnHK2Ornw"
+TOKEN = "ODM3MzYxNzAzNjUyOTUwMDU4.YIrb2w.UF7nIp1gYoO75Mdv43HlfeFPLBo"
 
 
-class YLBotClient(discord.Client):
-    async def on_ready(self):
-        print(f'{self.user} has connected to Discord!')
-        for guild in self.guilds:
-            print(
-                f'{self.user} подключились к чату:\n'
-                f'{guild.name}(id: {guild.id})')
-
-    async def on_member_join(self, member):
-        await member.create_dm()
-        await member.dm_channel.send(
-            f'Привет, {member.name}!'
-        )
-
-    async def on_message(self, message):
-        if message.author == self.user:
-            return
-        if "кот" in message.content.lower():
-            link = get('https://api.thecatapi.com/v1/images/search').json()[0]['url']
-            pikch = get(link).content
-            if pikch:
-                with open('pick.jpg', 'wb') as f:
-                    f.write(pikch)
-                await message.channel.send(file=discord.File(r'pick.jpg'))
-        elif 'собака' in message.content.lower() or 'пес' in message.content.lower():
-            link = get('https://dog.ceo/api/breeds/image/random').json()['message']
-            pikch = get(link).content
-            if pikch:
-                with open('pick.jpg', 'wb') as f:
-                    f.write(pikch)
-                await message.channel.send(file=discord.File(r'pick.jpg'))
-        else:
-            await message.channel.send("Спасибо за сообщение")
+def wait_and_send(seconds, minutes, hours):
+    time.sleep(int(seconds) + 60 * int(minutes) + 3600 * int(hours))
+    return True
 
 
-client = YLBotClient()
-client.run(TOKEN)
+class CommandTime(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='set_timer')
+    async def timer(self, ctx, hours, minutes, seconds):
+        if wait_and_send(seconds, minutes, hours):
+            await ctx.send("Время Х наступило!!!")
+
+
+bot = commands.Bot(command_prefix='!#')
+bot.add_cog(CommandTime(bot))
+bot.run(TOKEN)
